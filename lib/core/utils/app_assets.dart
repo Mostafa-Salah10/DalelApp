1e1500ad6a43b674df1ff71ceae3095cea4e09f5
Mostafa Salah <e01006828614@gmail.com>
2025-01-14 00:26:03 +0200

    refactor of images of project

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesOnBoaridng1
+  /// lib/assets/images/onBoaridng1.png
+  static const String assetsImagesOnBoaridng1 = "lib/assets/images/onBoaridng1.png";
+
+  /// Assets for assetsImagesOnBoaridng2
+  /// lib/assets/images/onBoaridng2.png
+  static const String assetsImagesOnBoaridng2 = "lib/assets/images/onBoaridng2.png";
+
+  /// Assets for assetsImagesOnBoaridng3
+  /// lib/assets/images/onBoaridng3.png
+  static const String assetsImagesOnBoaridng3 = "lib/assets/images/onBoaridng3.png";
+
+  /// Assets for assetsImagesSuccess
+  /// lib/assets/images/success.png
+  static const String assetsImagesSuccess = "lib/assets/images/success.png";
+
+  /// Assets for assetsImagesVector1
+  /// lib/assets/images/Vector1.svg
+  static const String assetsImagesVector1 = "lib/assets/images/Vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// lib/assets/images/Vector2.svg
+  static const String assetsImagesVector2 = "lib/assets/images/Vector2.svg";
+
+  /// Assets for assetsImagesVerefication
+  /// lib/assets/images/verefication.png
+  static const String assetsImagesVerefication = "lib/assets/images/verefication.png";
+}
+
